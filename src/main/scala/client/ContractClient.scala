4c2e39e8408b14c256264e9c16a4c852eca223b8
K-Singh <kiratgsingh@gmail.com>
2023-01-03 00:20:23 -0500

    Updated contract addresses, redid basic (not unit) tests

--- src/main/scala/client/ContractClient.scala
+++ src/main/scala/client/ContractClient.scala
@@ -14,7 +14,7 @@ object ContractClient extends App {
     "",
     RestApiErgoClient.getDefaultExplorerUrl(networkType))
 
-  val tokenIds: Seq[String] = Seq() // Add token id strings here
+  val tokenIds: Seq[String] = Seq("03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04") // Add token id strings here
 
   def printAddresses(ctx: BlockchainContext, optTokenId: Option[String]) = {
     val bondContract = ScriptGenerator.mkBondContract(ctx, optTokenId)
